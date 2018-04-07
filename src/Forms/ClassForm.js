04fa28e83d2e8c8441b8d392c7824fee90096d70
BoyWithSilverWings <agney.menons@live.com>
2018-04-07 22:59:12 +0530

    generate class form

---
+++ src/Forms/ClassForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Container, Form, Header, Button } from 'semantic-ui-react';
+
+class ClassForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = [
+      { key: 'u', text: 'Upper Primary', value: 'up' },
+      { key: '8', text: '8 th', value: 'eight' },
+      { key: '9', text: '9 th', value: 'nine' },
+    ]
+    this.state = {
+      section: 'up'
+    }
+    this.getMarks = this.getMarks.bind(this);
+    this.sectionChange = this.sectionChange.bind(this);
+  }
+  getMarks(event) {
+    const target = event.target;
+    const data = {
+      class: target.name,
+      division: target.division,
+      section: this.state.section
+    }
+    this.props.get(data);
+    this.props.history.push('/marks');
+  }
+  sectionChange(event, select) {
+    this.setState({
+      section: select.value
+    });
+  }
+  render() {
+    return (
+      <Container>
+        <Header>Class Form</Header>
+        <Form onSubmit={this.getMarks}>
+          <Form.Select fluid label='Section' value={this.state.section} options={this.options} placeholder='Section' name='section' onChange={this.sectionChange} />
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Class</label>
+              <input type="text" name="class" placeholder="Class Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Division</label>
+              <input type="text" name="division" placeholder="Division" />
+            </Form.Field>
+          </Form.Group>
+          <Container textAlign="center">
+            <Button type='submit' positive>Continue</Button>
+          </Container>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default ClassForm;
