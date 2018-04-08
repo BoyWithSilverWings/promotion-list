bc1de4eccfd78bb434bdc1df09825b6047f882bb
BoyWithSilverWings <agney.menons@live.com>
2018-04-08 18:16:38 +0530

    subjects incorporating dual change

--- src/Forms/Marks/subjects.js
+++ src/Forms/Marks/subjects.js
@@ -4,93 +4,102 @@ export default {
   nine: {
     te: 40,
     ce: 10,
-    1: [
-      {
-        name: "Language 1",
-        code: "lang1"
-      },
-      {
-        name: "Language 2",
-        code: "lang2"
-      },
-      {
-        name: "English",
-        code: "Eng",
-        te: 80,
-        ce: 20
-      },
-      {
-        name: "Hindi",
-        code: "Hin"
-      },
-      {
-        name: "Social Science",
-        code: "SS",
-        te: 80,
-        ce: 20
-      },
-      {
-        name: "Physics",
-        code: "Phy"
-      },
-      {
-        name: "Chemistry",
-        code: "Che"
-      },
-      {
-        name: "Biology",
-        code: "Bio"
-      },
-      {
-        name: "Maths",
-        code: "Mat",
-        te: 80,
-        ce: 20
-      },
-      {
-        name: "ICT",
-        code: "ICT"
-      }
-    ],
-    2: [
-      {
-        name: "Work Experience",
-        code: "WE"
-      },
-      {
-        name: "AE",
-        code: "AE"
-      },
-      {
-        name: "HPE",
-        code: "HPE"
-      }
-    ],
-    3: [
-      {
-        name: "CS",
-        code: "CS"
-      },
-      {
-        name: "C/C",
-        code: "C/C"
-      },
-      {
-        name: "GWS",
-        code: "GWS"
-      },
-      {
-        name: "RG",
-        code: "RG"
-      },
-      {
-        name: "LQ",
-        code: "LQ"
-      },
-      {
-        name: "SCA",
-        code: "SCA"
-      }
-    ]
+    1: {
+      dual: true,
+      list: [
+        {
+          name: "Language 1",
+          code: "lang1"
+        },
+        {
+          name: "Language 2",
+          code: "lang2"
+        },
+        {
+          name: "English",
+          code: "Eng",
+          te: 80,
+          ce: 20
+        },
+        {
+          name: "Hindi",
+          code: "Hin"
+        },
+        {
+          name: "Social Science",
+          code: "SS",
+          te: 80,
+          ce: 20
+        },
+        {
+          name: "Physics",
+          code: "Phy"
+        },
+        {
+          name: "Chemistry",
+          code: "Che"
+        },
+        {
+          name: "Biology",
+          code: "Bio"
+        },
+        {
+          name: "Maths",
+          code: "Mat",
+          te: 80,
+          ce: 20
+        },
+        {
+          name: "ICT",
+          code: "ICT"
+        }
+      ]
+    },
+    2: {
+      dual: false,
+      list: [
+        {
+          name: "Work Experience",
+          code: "WE"
+        },
+        {
+          name: "AE",
+          code: "AE"
+        },
+        {
+          name: "HPE",
+          code: "HPE"
+        }
+      ]
+    },
+    3: {
+      dual: true,
+      subjects: [
+        {
+          name: "CS",
+          code: "CS"
+        },
+        {
+          name: "C/C",
+          code: "C/C"
+        },
+        {
+          name: "GWS",
+          code: "GWS"
+        },
+        {
+          name: "RG",
+          code: "RG"
+        },
+        {
+          name: "LQ",
+          code: "LQ"
+        },
+        {
+          name: "SCA",
+          code: "SCA"
+        }
+      ]
+    }
   }
 };
