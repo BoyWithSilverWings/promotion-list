4838fccbae3fb7db5f936a43ce4e35169fae9fb1
BoyWithSilverWings <agney.menons@live.com>
2018-04-08 21:40:00 +0530

    set default value on general screen

--- src/Forms/GeneralForm.js
+++ src/Forms/GeneralForm.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Container, Header, Form, Button } from 'semantic-ui-react';
+import React from "react";
+import { Container, Header, Form, Button } from "semantic-ui-react";
 
 class GeneralForm extends React.Component {
   constructor(props) {
     super(props);
+    this.name = window.localStorage.getItem("SCHOOL_NAME");
+    this.days = window.localStorage.getItem("DAYS");
     this.getSchoolName = this.getSchoolName.bind(this);
   }
   getSchoolName(event) {
     const data = {
       name: event.target.schoolName.value,
       days: event.target.days.value
-    }
+    };
+    window.localStorage.setItem("SCHOOL_NAME", data.name);
+    window.localStorage.setItem("DAYS", data.days);
     this.props.get(data);
-    this.props.history.push('/class');
+    this.props.history.push("/class");
   }
   render() {
     return (
@@ -21,14 +25,27 @@ class GeneralForm extends React.Component {
         <Form onSubmit={this.getSchoolName}>
           <Form.Field required>
             <label>School Name</label>
-            <input placeholder='School Name' name="schoolName" required />
+            <input
+              placeholder="School Name"
+              name="schoolName"
+              required
+              defaultValue={this.name}
+            />
           </Form.Field>
           <Form.Field>
             <label>Total Number of School Days</label>
-            <input type="number" placeholder="Days" name="days" />
+            <input
+              type="number"
+              placeholder="Days"
+              name="days"
+              required
+              defaultValue={this.days}
+            />
           </Form.Field>
           <Container textAlign="center">
-            <Button type='submit' positive>Continue</Button>
+            <Button type="submit" positive>
+              Continue
+            </Button>
           </Container>
         </Form>
       </Container>
