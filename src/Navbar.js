37b44bdb7e1de58d0493399ae3d2bf0549416c5d
BoyWithSilverWings <agney.menons@live.com>
2018-04-08 23:31:11 +0530

    adding link to /

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Segment, Header } from "semantic-ui-react";
+import { Link, withRouter } from "react-router-dom";
 
 class Navbar extends React.PureComponent {
   render() {
     return (
       <Segment inverted textAlign="center">
-        <Header>PROMOTIONS LIST</Header>
+        <Link to="/" className="unstyled-link">
+          <Header className="white">PROMOTIONS LIST</Header>
+        </Link>
       </Segment>
     );
   }
 }
 
-export default Navbar;
+export default withRouter(Navbar);
